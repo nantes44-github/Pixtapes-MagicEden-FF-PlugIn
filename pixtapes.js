b5381e642f19ae11452772fa2ca6bc7658377c5f
nantes44-github <56247405+nantes44-github@users.noreply.github.com>
2022-02-15 23:54:42 +0100

    Add files via upload

--- pixtapes.js
+++ pixtapes.js
@@ -16,14 +16,15 @@ function titleModified() {
     
     // 4. This will be called after the response is received
     xhr.onload = function() {
-        if (xhr.status != 200) { // analyze HTTP status of the response
+        if (xhr.status != 200 && xhr2.status != 200) { // analyze HTTP status of the response
           console.log(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
         } else { // show the result
           var parser = new DOMParser();
           var xmlDoc = parser.parseFromString(xhr.responseText, "text/html"); 
+          //console.log(xhr.responseText);
           var rarity = xmlDoc.evaluate("(//strong[text()='PixTape']/../em)",xmlDoc,null,XPathResult.STRING_TYPE,null);
           console.log(`${rarity.stringValue}`);
-          document.getElementsByClassName("tw-m-0 item-title tw-text-xl")[0].textContent =document.getElementsByClassName("tw-m-0 item-title tw-text-xl")[0].textContent + " ||" + rarity.stringValue + "||";
+          document.getElementsByClassName("tw-m-0 item-title tw-text-xl")[0].innerHTML =document.getElementsByClassName("tw-m-0 item-title tw-text-xl")[0].innerHTML + "<br>" + rarity.stringValue;
         }
       };
       
@@ -40,50 +41,65 @@ function titleModified() {
         console.log(`Request failed ${xhr.status} ${xhr.response}`);
       };
     }
+    console.log("Fin de la partie pixtapes");
+    // 4. This will be called after the response is received    
 }
 
-/*
-window.onchange = function() {
-    var titleEl = document.getElementsByTagName("title")[0];
-    var docEl = document.documentElement;
+function pixcase() {
+    console.log("plug-in pixcases"); 
+    var title = document.getElementsByTagName("title")[0].innerHTML;
+    console.log(title);
+    var tapeNum = title.split(" |")[0].split("#")[1];
+    console.log(tapeNum);
 
-    if (docEl && docEl.addEventListener) {
-        docEl.addEventListener("DOMSubtreeModified", function(evt) {
-            var t = evt.target;
-            if (t === titleEl || (t.parentNode && t.parentNode === titleEl)) {
-                titleModified();
-                var title = document.getElementsByTagName("title")[0].innerHTML;
-                console.log(title);
-            }
-        }, false);
-    } else {
-        document.onpropertychange = function() {
-            if (window.event.propertyName == "title") {
-                titleModified();
-            }
-        };
+    if (title.split(" |")[0].split("#")[0]=="PixTape ") {
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://pixtapes.com/case/"+tapeNum, true);
+    xhr.setRequestHeader('Content-type', 'text/html');
+    xhr.send();
+    
+    // 4. This will be called after the response is received
+    xhr.onload = function() {
+        if (xhr.status != 200) { // analyze HTTP status of the response
+          console.log(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+        } else { // show the result
+          var parser = new DOMParser();
+          var xmlDoc = parser.parseFromString(xhr.responseText, "text/html"); 
+          //console.log(xhr.responseText);
+          var caseUrl = xmlDoc.evaluate("(//a[text()='Magic Eden']/@href)",xmlDoc,null,XPathResult.STRING_TYPE,null);
+          console.log(caseUrl.stringValue);
+          document.getElementsByClassName("tw-m-0 item-title tw-text-xl")[0].innerHTML  =document.getElementsByClassName("tw-m-0 item-title tw-text-xl")[0].innerHTML + "<br><a href='"+caseUrl.stringValue+"'>>>Go to associated PixCase<<</a>";
+        }
+      };
+      
+      xhr.onprogress = function(event) {
+        if (event.lengthComputable) {
+          console.log(`Received ${event.loaded} of ${event.total} bytes`);
+        } else {
+          console.log(`Received ${event.loaded} bytes`); // no Content-Length
+        }
+      
+      };
+      
+      xhr.onerror = function() {
+        console.log(`Request failed ${xhr.status} ${xhr.response}`);
+      };
     }
-};
-*/
+    console.log("Fin de la partie pixcases");
+    // 4. This will be called after the response is received    
+}
+
+
+
+
+
+
+
 
 window.onload = function() {
-    var titleEl = document.getElementsByTagName("title")[0];
-    var docEl = document.documentElement;
-
-    if (docEl && docEl.addEventListener) {
-        docEl.addEventListener("DOMSubtreeModified", function(evt) {
-            var t = evt.target;
-            if (t === titleEl || (t.parentNode && t.parentNode === titleEl)) {
-                titleModified();
-                var title = document.getElementsByTagName("title")[0].innerHTML;
-                console.log(title);
-            }
-        }, false);
-    } else {
-        document.onpropertychange = function() {
-            if (window.event.propertyName == "title") {
-                titleModified();
-            }
-        };
-    }
+    console.log("On load");
+    titleModified();
+    pixcase();  
 };
